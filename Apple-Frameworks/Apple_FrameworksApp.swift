a6d77ed58cab9ca092f1588ce084d13609d50c2d
mshayan-experlabs <138456600+mshayan-experlabs@users.noreply.github.com>
2024-11-02 11:03:14 +0500

    Initial Commit

---
+++ Apple-Frameworks/Apple_FrameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_FrameworksApp.swift
+//  Apple-Frameworks
+//
+//  Created by Muhammad Shayan on 02/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct Apple_FrameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
